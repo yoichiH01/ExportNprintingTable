1290e79458e62a1baa2fbc365841c1703dfb1ab2
yoichiH01 <46386743+yoichiH01@users.noreply.github.com>
2019-01-05 13:14:50 +1100

    Add files via upload

---
+++ ExportNprintingTable.ps1
@@ -0,0 +1,9 @@
+﻿#ExportNprintingTable version 1.0 Author: Yoichi H #
+$PosgresLocation = "C:\Program Files\NPrintingServer\pgsql\bin"
+$PosgresLocation
+$sql = "C:\yheTemp\ExportNprintingTable.sql"
+$sql
+
+Start-Process -FilePath $("$PosgresLocation\psql.exe") -ArgumentList "-h localhost -p 4992 -d nprinting -U postgres -f $sql"
+
+Write-Host "Script execution Finished `r`n"
