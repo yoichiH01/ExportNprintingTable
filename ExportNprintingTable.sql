c02a4ca7f0243baaa13c8cc2d069360ed44f1d2d
yoichiH01 <46386743+yoichiH01@users.noreply.github.com>
2019-01-19 06:06:45 +1100

    Update ExportNprintingTable.sql

--- ExportNprintingTable.sql
+++ ExportNprintingTable.sql
@@ -1,4 +1,23 @@
-/*#ExportNprintingTable version 1.0 Author: Yoichi H*/
+/*
+MIT License
+Copyright (c) 2019 Yoichi Hirotake
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE`
+SOFTWARE.
+ExportNprintingTable version 1.0
+*/
 copy public."acl_entry" To 'C:\yheTemp\Nprinting\acl_entry.txt' WITH CSV HEADER;
 copy public."action" To 'C:\yheTemp\Nprinting\action.txt' WITH CSV HEADER;
 copy public."app" To 'C:\yheTemp\Nprinting\app.txt' WITH CSV HEADER;
